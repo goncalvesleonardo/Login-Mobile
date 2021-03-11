b5b3c9b7dc970387eef764fc6c5f7717bd944145
Leonardo Oliveira Goncalves <leonardo.goncalves@totvs.com.br>
2021-03-11 00:42:10 -0300

    ajuste navegação

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Login from '../pages/login/login';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e39' },
+    }}
+  >
+    <Auth.Screen name="Login" component={Login} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
